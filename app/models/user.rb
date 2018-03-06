b4cca1407c97582e137355cfae721c2cabe4f95d
Hammad Asad <hammad_asad@msn.com>
2018-03-06 13:10:37 -0500

    Added some validations and user storage with password_digest creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
+    validates :email, presence: true
     has_many :user_destination_list
     has_many :destination, through: :user_destination_list
 end
